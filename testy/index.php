bb327d13be2c119f5b4e2b2ccea0800cb9d2387a
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2016-05-24 20:58:12 +0200

    tests mysql 1

--- testy/index.php
+++ testy/index.php
@@ -43,7 +43,7 @@ require $phplocal.'/scripts/verification.php';
 			<h1 class="start-headline">Testy aktualne
 				<a href="<?php echo $httplocal;?>/testy_zakonczone"><div class="start-bookmark">Testy zakończone</div></a>
 				<a><div class="start-bookmark-open">Testy aktualne</div></a>
-			</h1><br>
+			</h1>
 			<table class="start-table">
 				<tr><th class="start-table-name">Nazwa</th><th class="start-table-date">Przedmiot</th><th class="start-table-date">Początek</th><th class="start-table-date">Koniec</th><th class="start-table-action">Akcja</th></tr>
 <?php
@@ -54,10 +54,21 @@ require $phplocal.'/scripts/verification.php';
 		}
 		
 		mysqli_set_charset($conn,"utf8");
+		
+		$sql = "SELECT test_id, test_name, subject_name, test_start, test_stop FROM SC_tests LEFT JOIN SC_subjects ON test_subj=subjectsID";
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo '<tr><td class="start-table-name"><a href="'.$httplocal.'/info/'.$row["test_id"].'">'.$row["test_name"].'</a></td><td class="start-table-date">'.$row["subject_name"].'</td><td class="start-table-date">'.date("d-m-Y H:i", strtotime($row["test_start"])).'</td><td class="start-table-date">'.date("d-m-Y H:i", strtotime($row["test_stop"])).'</td><td class="start-table-action"><a href="'.$httplocal.'/info/'.$row["test_id"].'"><div class="start-table-button">Info</div></a><a><div class="start-table-button">Rozpocznij</div></a></td></tr>';
+			}
+		} else {
+			echo '<tr><td class="start-table-name" colspan="5">Brak testów bieżących</td></tr>';
+		}
 
 				
 ?>				
-				<tr><td class="start-table-name"><a>Konkurs Sinus i cosunus</a></td><td class="start-table-date">Matematyka</td><td class="start-table-date">13-12-2015 20:15</td><td class="start-table-date">13-12-2015 23:15</td><td class="start-table-action"><a><div class="start-table-button" style="display: inline; padding: 0 7px;">Info</div></a> <a><div class="start-table-button" style="display: inline; padding: 0 7px;">Rozpocznij</div></a></td></tr>
+				<!--<tr><td class="start-table-name"><a>Konkurs Sinus i cosunus</a></td><td class="start-table-date">Matematyka</td><td class="start-table-date">13-12-2015 20:15</td><td class="start-table-date">13-12-2015 23:15</td><td class="start-table-action"><a><div class="start-table-button" style="display: inline; padding: 0 7px;">Info</div></a><a><div class="start-table-button" style="display: inline; padding: 0 7px;">Rozpocznij</div></a></td></tr>-->
 			</table>
 		</div>
 		<?php include $phplocal.'/scripts/footer.php';?>
