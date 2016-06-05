dd29ad222a5369121965eaff9ef5b61a02bd1836
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2016-06-05 15:01:18 +0200

    quiz css + html

--- scripts/tests/quiz.php
+++ scripts/tests/quiz.php
@@ -1,78 +1,78 @@
+<?php
+/***************************************
+/
+/	Sinus cosinus - Patryk Kisielewski
+/
+/	Test typu: Quiz
+/
+***************************************/
+if(!isset($test)){
+	exit('Error: No testID');
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Sinus cosinus - test</title>
+		<title>Sinus cosinus - Rozwiąż test</title>
 		<meta charset="UTF-8">
-		<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" href="<?php echo $httplocal;?>/images/sincos.ico"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo $httplocal;?>/css/main.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo $httplocal;?>/css/tests/quiz.css">
+		<meta name="author" content="Patryk Kisielewski">
+		<meta name="description" content="Platforma edukacyjna">
 	</head>
-	<body style="margin: 0; background-color: #ebebe0;font-family:Lucida Sans Unicode;">
-		<div style="height: 60px; width: 100%; margin: 0 auto; background-color: #ff9900;">
-		<div style="height: 60px; width: 745px; margin: 0 auto;"><img src="/logo/logo_sincos.png" style=""/></div>
-		</div>
-		<div style="height: 3px; width: 100%; margin: 0 auto; background-color: #bebea7;">
-		</div>
-		<div style="height: 14px; width: 745px; margin: 0 auto; font-size: 13px; color: #6699ff;">
-		strona główna / wyniki / ranking / user1 / test2E1_1
-		</div>
-		<div style="height: 424px; width: 755px; margin: 30px auto;">
-		<div id="big_box" style="height: 420px; width: 420px; margin: 0 5px; border-style: solid; border-width: 2px; border-color: #a6a6a6; border-radius: 5px; background-color: #fff; float: left;">
-		
-		</div>
-		
-		<div style="height: 80px; width: 100px; margin: 0 0 0 5px; float: left; background-color: #cc3300; color: white; text-align: center;">
-		<span style="font-size: 35px;font-weight: bold;">1</span><br><span style="letter-spacing: 3px;">Runda</span>
-		
-		</div>
-		<div style="height: 80px; width: 200px; margin: 0; float: left; background-color: #ff794d; color: white; text-align: center;">
-		<span style="font-size: 25px;">Pytanie <span id="pytanie">1</span></span><br>
-		<div style="line-height:16px;font-size: 13px;">
-		<div style="width: 180px; margin: 0 auto;"><div style="float: left;width: 110px;text-align: center;">Koniec rundy: </div><div id="runda_timers" style="width: 70px; float: left;">- s</div>
-		</div>
-		
-		<div style="width: 180px; margin: 0 auto;"><div style="float: left;width: 110px;text-align: center;">Koniec pytania: </div><div id="timers" style="width: 70px; float: left;">- s</div>
-		</div></div>
-		</div><br>
-		<div  style="height: 5px; width: 300px; margin: 0 0 0 5px; float: left; ">
-		<div id="prog_runda" style="height: 5px; width: 100px; margin: 0; background-color: red; ">
-		</div>
-		</div>
-		
-		<div id="small_box" style="height: 150px; width: 300px; margin: 10px 5px 0 5px; border-style: solid; border-width: 2px; border-color: #a6a6a6; border-radius: 5px; float: left; text-align: center;">
-		
-		</div>
-		<div id="box" style="height: 80px; width: 300px; margin: 10px 0 0 5px; float: left; color: white; text-align: center;text-decoration: none;">
-		
-			<div id="button1" style="background-color: #000099; height: 80px; margin: 5px 5px 5px 0; width: 145px; float: left; cursor: pointer;">
-			
+	<body>
+		<div class="area-for-logo"></div>
+		<div class="headline">
+			<div class="headline-content">
+				<a href="<?php echo $httplocal;?>/"><img src="<?php echo $httplocal;?>/images/logo_sincos.png" class="headline-logo"/></a>
+			<div class="user-menu">
+					<div class="user-menu-head"><?php echo $users_login;?></div>
+					<a href="<?php echo $httplocal;?>/logout" class="user-menu-link"><div class="user-menu-content">Wyloguj</div></a>
+					<a href="<?php echo $httplocal;?>/konto" class="user-menu-link"><div class="user-menu-content">Konto</div></a>
+				</div>
 			</div>
-			<div id="button2" style="background-color: #000099; height: 80px; margin: 5px 0 5px 5px; width: 145px; float: left; cursor: pointer;">
-			
+		</div>
+		<div class="headline-navi-bar-area">
+			<div class="headline-navi-bar">
+				<a href="<?php echo $httplocal;?>/" class="headline-navi-link">strona główna</a> / 
+				<a href="<?php echo $httplocal;?>/start"  class="headline-navi-link">start</a> / 
+				<a href="<?php echo $httplocal;?>/testy"  class="headline-navi-link">testy</a> / 
+				<a href="<?php echo $httplocal;?>/konto" class="headline-navi-link"><?php echo $users_log;?></a>
 			</div>
-			<div id="button3" style="background-color: #000099; height: 80px; margin: 5px 5px 5px 0; width: 145px; float: left; cursor: pointer;">
-			
+		</div>
+
+		<div class="quiz-container">
+			<div class="quiz-left-panel">
+				<div id="box1" class="quiz-bigbox"></div>
 			</div>
-			<div id="button4" style="background-color: #000099; height: 80px; margin: 5px 0 5px 5px; width: 145px; float: left; cursor: pointer;">
-			
+			<div class="quiz-right-panel">
+				<div class="quiz-round-box">
+					<div class="quiz-round-number">1</div>
+					<div class="quiz-round-label">Runda</div>
+				</div>
+				<div class="quiz-counters-box">
+					<div class="quiz-question">Pytanie 1</div>
+					<div class="quiz-counter-line">
+						<div class="quiz-counter-label">Koniec rundy:</div>
+						<div class="quiz-counter">- s</div>
+					</div>
+					<div class="quiz-counter-line">
+						<div class="quiz-counter-label">Koniec pytania:</div>
+						<div class="quiz-counter">- s</div>
+					</div>
+				</div>
+				<div class="quiz-progressbar-round"></div>
+				<div class="quiz-smallbox-div"><div class="quiz-smallbox"></div></div>
+				<div class="quiz-button-grid"><div class="quiz-button">A</div></div>
+				<div class="quiz-button-grid quiz-button-grid-right"><div class="quiz-button">B</div></div>
+				<div class="quiz-button-grid"><div class="quiz-button">C</div></div>
+				<div class="quiz-button-grid quiz-button-grid-right"><div class="quiz-button">D</div></div>
 			</div>
-		
-		</div>
-		
-		
-		</div>
-		
-		<div style="height: 5px; width: 745px; margin: 20px auto 0;">
-		
-		<div id="progress_barr_progress" style="height: 5px; width: 0px; float:left;background-color: #000099;"></div>
-		<div id="progress_barr_bg" style="height: 2px; width: 740px; float:left;background-color: #a6a6a6;"></div>
-		
-		</div>
-		
-		
-		<div style="height: 14px; width: 745px; margin: 30px auto 0; font-size: 13px; color: #6699ff;text-align: center;">
-		2015 - KIMBI Projects
-		</div>
-		
+			<div class="quiz-progressbar-question-box">
+				<div class="quiz-progressbar-question"></div><div class="quiz-progressbar-question-bg"></div>
+			</div>
+		</div>		
+		<?php include $phplocal.'/scripts/footer.php';?>
 	</body>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="test.js"></script>
 </html>		
