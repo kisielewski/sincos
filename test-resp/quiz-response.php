179c7d0fd7e5e219f1d7dde7653dffd4be61417e
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2016-06-07 01:09:00 +0200

    quiz test quiz-response

---
+++ test-resp/quiz-response.php
@@ -0,0 +1,13 @@
+<?php
+/***************************************
+/
+/	Sinus cosinus - Patryk Kisielewski
+/
+/	Quiz odpowiedzi serwera
+/
+***************************************/
+header('Content-Type: application/json');
+sleep(2);
+$tmp = ["status" => "create"];
+echo json_encode($tmp);
+?>
